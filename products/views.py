669896304ada27352109cec2c8e7355c2f21a568
Alexey <leventcov.a.v@gmail.com>
2021-12-23 15:16:11 +0600

    commit lesson4

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from products.models import ProductCategory, Product
 # Create your views here.
 def index(request):
     context = {
@@ -9,24 +9,9 @@ def index(request):
 
 def products(request):
     context = {
-        'title': 'Store - Каталог'
+        'title': 'Store - Каталог',
+        'categories': ProductCategory.objects.all(),
+        'products': Product.objects.all(),
     }
     return render(request, 'products/products.html',context)
 
-def test_context(request):
-    context ={
-        'title': 'store',
-        'header': 'welcome!',
-        'username': 'Иван',
-        'products': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090.00},
-            {'name': 'Синяя куртка The North Face', 'price': 23725.00},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390.00},
-        ],
-        #'promotion':True,
-        'products_of_promotion': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 3000.00},
-        ]
-
-    }
-    return render(request,'products/test-context.html',context)
